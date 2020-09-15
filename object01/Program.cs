052ac5a440673e3866aa4a04820c9447b7700a3b
bettilaszlo <laszlobetti3@gmail.com>
2020-09-15 09:29:01 +0200

    Add project files.

---
+++ object01/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace object01
+{
+    class Ember
+    {
+        public string nev;
+        public int eletkor;
+
+        public string Bemutatkozas()
+        {
+            //string valasz = $"{nev} vagyok {eletkor} éves.";
+            //return valasz;
+
+            return $"{nev} vagyok {eletkor} éves.";
+      
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ember pisti = new Ember();
+            Ember eva = new Ember();
+
+            pisti.nev = "Nagy Pisti";
+            pisti.eletkor = 20;
+
+            eva.nev = "Nagy Éva";
+            eva.eletkor = 18;
+
+            pisti.eletkor++;
+
+
+            Console.WriteLine(pisti.Bemutatkozas());
+            Console.WriteLine(eva.Bemutatkozas());
+
+        Console.ReadKey();
+        }
+    }
+}
